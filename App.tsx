10886794d206ddd6873d1a861c009334090c471a
Vaidika Vadhvaniya <vaidika.vadhvaniya@lapn222331.local>
2022-06-28 18:46:13 +0530

    feat: Application architecture

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+
+import React from 'react';
+import 'react-native-gesture-handler';
+
+import { NavigationContainer } from '@react-navigation/native';
+
+import { store } from './src/Redux';
+import Navigation from './src/navigators';
+import { Provider } from 'react-redux';
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Navigation />
+      </NavigationContainer>
+    </Provider>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
